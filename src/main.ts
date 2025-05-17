608de8061f11668428bc45c11b843fdc1db8b33b
bengo <171782+gobengo@users.noreply.github.com>
2025-05-17 04:36:42 -0700

    remove log

--- src/main.ts
+++ src/main.ts
@@ -50,7 +50,6 @@ export async function run() {
     let lastName
     for (const file of files) {
       const isDirectory = lstatSync(file).isDirectory()
-      console.debug('isDirectory', isDirectory, { file })
       if (isDirectory) continue
       const name = file.split('/').pop() || ''
       lastName = name
